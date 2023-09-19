f34fd902ff218bc343ffeef8facf9de2293df327
Joshua <lab.joshacc@gmail.com>
2023-09-19 11:48:19 +0800

    installed mysql

---
+++ index.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "SysAcc",
+    password: "password",
+    database: "amsdatabase"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
