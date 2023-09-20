8636a0cdd885bb429c4030fe8e5f1a90c5ea0fe7
Joshua <lab.joshacc@gmail.com>
2023-09-20 16:24:48 +0800

    add all commit

--- server.js
+++ server.js
@@ -1,30 +1,58 @@
 const express = require('express');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { authenticateUser } = require('./Components/Login');
+const cors = require('cors');
 
-const app = express()
-const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(bodyParse.json());
+const JW_SECRET = 'ad165b11320bc91501ab08613cc3a48a62a6caca4d5c8b14ca82cc313b3b96cd'; // Replace with your secret key
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.sendStatus(401); // Unauthorized if no token
+  }
+
+  jwt.verify(token, JW_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403); // Forbidden if token is invalid
+    }
+    req.user = user;
+    next(); // Continue to the next middleware or route handler
+  });
+}
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Apply API authentication middleware to all routes
+app.use(authenticateToken);
 
 app.post('/Components/Login', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const authenticationResult = await authenticateUser(username, password);
-        if (authenticationResult === 'Authentication successful.') {
-            res.status(200).json({ message: 'Authentication successful.' });
-        } else {
-            res.status(401).json({ message: authenticationResult });
+    app.post('/Components/Login', async (req, res) => {
+        try {
+          const { username, password } = req.body;
+          const authenticationResult = await authenticateUser(username, password);
+          if (authenticationResult === 'Authentication successful.') {
+            const token = jwt.sign({ username }, JW_SECRET);
+            res.status(200).json({ message: 'Authentication successful.', token });
+          } else {
+            res.status(401).json({ message: authenticationResult }); // Return error as JSON
+          }
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: 'Internal server error.' }); // Return error as JSON
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error.' });
-    }
-});
+    })
+      });
 
+// ... (other routes and server listening)
 
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
