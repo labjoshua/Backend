8780a58986a50e57771c57973ae02dc641e987ae
Joshua <lab.joshacc@gmail.com>
2023-09-21 17:45:04 +0800

    Will now implement JWT Authentication, incase something break revert here!

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
+//Implementaion of JWT Auth
+
 // Route for the login authentication
 app.post('/Components/Login', async (req, res) => {
   try {
