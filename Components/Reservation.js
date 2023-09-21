c7241d5c378d16839b84fa109c04e3f7a5a270df
Joshua <lab.joshacc@gmail.com>
2023-09-21 21:54:04 +0800

    Okay na lahat functioning na kahit ung mga response ng mga server. When made changes revert here kapag nagkamali!

--- Components/Reservation.js
+++ Components/Reservation.js
@@ -1,26 +1,28 @@
 const pool = require('./db_Connection');
 
 exports.ReserveGuest = async (guestID, roomID, reservationDateFrom, reservationDateTo, reservationStatus) => {
-    const sql = `INSERT INTO amsreservation(guestID, roomID, reservationDateFrom, reservationDateTo, reservationStatus)
-    VALUES (?, ?, ?, ?, ?)`;
+    try {
+        const sql = `INSERT INTO amsreservation(guestID, roomID, reservationDateFrom, reservationDateTo, reservationStatus)
+        VALUES (?, ?, ?, ?, ?)`;
 
-    const values = [
-        guestID,
-        roomID,
-        reservationDateFrom,
-        reservationDateTo,
-        reservationStatus
-    ];
+        const values = [
+            guestID,
+            roomID,
+            reservationDateFrom,
+            reservationDateTo,
+            reservationStatus
+        ];
 
-    return new Promise((resolve, reject) => {
-        pool.execute(sql, values, (err, results) => {
-            if (err) {
-                console.error('Error inserting data:', err);
-                reject('Error inserting data');
-            } else {
-                console.log('Data inserted successfully:', results);
-                resolve('Reservation successful');
-            }
-        });
-    });
+        const [results] = await pool.execute(sql, values);
+
+        if (results.affectedRows === 1) {
+            // Successfully inserted one row
+            return 'Reservation successful';
+        } else {
+            return 'Error inserting data';
+        }
+    } catch (err) {
+        console.error('Error inserting data:', err);
+        return 'Internal server error.';
+    }
 };
