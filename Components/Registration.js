9bb75a7e44dfe14cc6bae0dbc9075f1c4b333bc3
Joshua <lab.joshacc@gmail.com>
2023-09-21 21:47:17 +0800

    Nag reresponse na ang server sa registration endpoint. fix ung reservation, revert here pag nagkamali

--- Components/Registration.js
+++ Components/Registration.js
@@ -1,37 +1,39 @@
 const pool = require('./db_Connection');
 const crypto = require('crypto');
 
-exports.registerGuest = async (guestName, guestContactInfo, guestEmail, EncodedDate, userName, userPass) => {
-    // Hash the userPass using SHA-256 and convert it to uppercase
-    const hashedUserPass = hashPassword(userPass).toUpperCase();
-
-    const sql = `INSERT INTO amsguests (guestName, guestContactInfo, guestEmail, EncodedDate, userName, userPass)
-    VALUES (?, ?, ?, ?, ?, ?)`;
-
-    const values = [
-        guestName,
-        guestContactInfo,
-        guestEmail,
-        EncodedDate,
-        userName,
-        hashedUserPass, // Use the hashed password here
-    ];
-
-    return new Promise((resolve, reject) => {
-        pool.execute(sql, values, (err, results) => {
-            if (err) {
-                console.error('Error inserting data:', err);
-                reject('Error inserting data');
-            } else {
-                console.log('Data inserted successfully:', results);
-                resolve('Registration successful');
-            }
-        });
-    });
-};
-
 function hashPassword(password) {
     const sha256 = crypto.createHash('sha256');
     sha256.update(password);
     return sha256.digest('hex');
 }
+
+exports.registerGuest = async (guestName, guestContactInfo, guestEmail, EncodedDate, userName, userPass) => {
+    try {
+        // Hash the userPass using SHA-256 and convert it to uppercase
+        const hashedUserPass = hashPassword(userPass).toUpperCase();
+
+        const sql = `INSERT INTO amsguests (guestName, guestContactInfo, guestEmail, EncodedDate, userName, userPass)
+        VALUES (?, ?, ?, ?, ?, ?)`;
+
+        const values = [
+            guestName,
+            guestContactInfo,
+            guestEmail,
+            EncodedDate,
+            userName,
+            hashedUserPass, // Use the hashed password here
+        ];
+
+        const [results] = await pool.execute(sql, values);
+
+        if (results.affectedRows === 1) {
+            // Successfully inserted one row
+            return 'Registration successful';
+        } else {
+            return 'Error inserting data';
+        }
+    } catch (err) {
+        console.error('Error inserting data:', err);
+        return 'Internal server error.';
+    }
+};
