ea29b7e47d8eb02c8391ea4e021f79610684dd3a
Joshua <lab.joshacc@gmail.com>
2023-10-03 01:48:56 +0800

    Mema lang

--- Components/Reservation.js
+++ Components/Reservation.js
@@ -1,28 +1,26 @@
 const pool = require('./db_Connection');
 
 exports.ReserveGuest = async (guestID, roomID, reservationDateFrom, reservationDateTo, reservationStatus) => {
-    try {
-        const sql = `INSERT INTO amsreservation(guestID, roomID, reservationDateFrom, reservationDateTo, reservationStatus)
-        VALUES (?, ?, ?, ?, ?)`;
+    const sql = `INSERT INTO amsreservation(guestID, roomID, reservationDateFrom, reservationDateTo, reservationStatus)
+    VALUES (?, ?, ?, ?, ?)`;
 
-        const values = [
-            guestID,
-            roomID,
-            reservationDateFrom,
-            reservationDateTo,
-            reservationStatus
-        ];
+    const values = [
+        guestID,
+        roomID,
+        reservationDateFrom,
+        reservationDateTo,
+        reservationStatus
+    ];
 
-        const [results] = await pool.execute(sql, values);
-
-        if (results.affectedRows === 1) {
-            // Successfully inserted one row
-            return 'Reservation successful';
-        } else {
-            return 'Error inserting data';
-        }
-    } catch (err) {
-        console.error('Error inserting data:', err);
-        return 'Internal server error.';
-    }
+    return new Promise((resolve, reject) => {
+        pool.execute(sql, values, (err, results) => {
+            if (err) {
+                console.error('Error inserting data:', err);
+                reject('Error inserting data');
+            } else {
+                console.log('Data inserted successfully:', results);
+                resolve('Reservation successful');
+            }
+        });
+    });
 };
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://arthursplace-mabini.ddns.net',
+  origin: 'http://localhost:3000',
   credentials: true,
 };
 
